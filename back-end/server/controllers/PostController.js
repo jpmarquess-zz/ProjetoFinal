fdb5713f20af4c6322b224d5ba4e58c48fc0970e
João Marques <jpmarques1099@gmail.com>
2019-06-05 20:00:26 +0100

    Update

---
+++ back-end/server/controllers/PostController.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'social'
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Gets user
+exports.index = function (request, response) {
+    query = "SELECT * FROM users";
+
+    connection.query(query, function (error, results, fields) {
+        if (!error)
+            response.send(results);
+        else
+            console.log('query failed');
+    })
+}
+
+// Gets user by id
+exports.post = function (request, response) {
+    id = request.params.id;
+    query = "SELECT * FROM users WHERE user_id = ?";
+
+    connection.query(query, id, function (error, results, fields) {
+        if (!error)
+            response.send(results);
+        else
+            console.log('query failed');
+    })
+}
